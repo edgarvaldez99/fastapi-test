ed6734ca20f578ddd0538dd98b8e6a569c2a2a34
Edgar <edgarvaldez99@gmail.com>
2021-08-28 02:12:41 -0400

    chore: Add tests, linters and type checking

---
+++ noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+@nox.session(python=["3.7", "3.8"])
+def tests(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "report")
+
+@nox.session
+def lint(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("black", "--check", "app/")
+    session.run("isort", "--recursive", "--check-only", "app")
+    session.run("flake8", "app")
+
+@nox.session
+def typing(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("mypy", "app")
